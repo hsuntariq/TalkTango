dd469b0c916fa3a0f9a08caae7145a2ab7253203
Hassan Tariq <128592715+hsuntariq@users.noreply.github.com>
2024-05-28 23:38:53 +0500

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -22,7 +22,6 @@ app.use(cors(
 const server = http.createServer(app);
 
 
-mongoose.connect('mongodb+srv://yousaf:test123@cluster0.g4i5dey.mongodb.net/test?retryWrites=true&w=majority');
 
 
 app.get('/',(req,res)=>{
